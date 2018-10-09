84e9850e8501b09f80035766bc3c7ccd07dd1743
Panagiotis Papaioannou <gekko1990@gmail.com>
2018-10-09 11:22:49 +0300

    cn23 more

--- CN23.py
+++ CN23.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 from flask import Flask, render_template, jsonify, json
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import io, requests, os, sys
+import io, requests
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -10,6 +10,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.pdfmetrics import registerFontFamily
 from reportlab.pdfbase.ttfonts import TTFont
 import urllib, pandas as pd, sendgrid
+import os, re, os.path
 
 #sys.path.insert(0, '/opt/torus-engine/torus/')
 ParentPath = os.path.realpath('Users_OrdersUploads/')
@@ -64,20 +65,20 @@ def parseOrders(userId, dataSender, wixUserEmail):
 
                 #amazon
                 if df.columns[0] == 'order-id':
-                    dataReceiver = {'Name': str(row['buyer-name']), 'Business': str(row['buyer-name']), 'Street': str(row['ship-address-1']),
+                    dataReceiver = {'Name': str(row['recipient-name']), 'Business': '', 'Street': str(row['ship-address-1']),
                                 'TelNo': str(row['buyer-phone-number']), 'PostCode': str(row['ship-postal-code']), 'City': str(row['ship-city']), 'Country': str(row['ship-country'])}
                     redProdDescr = row['product-name']
                     prodPriceIn = "%.2f" % round(row['Price'],2)
-                    dataProductsInfo = {'prodDescription': str(redProdDescr[0:25]), 'prodQuantity': str(row['quantity-purchased']), 'prodWeight': str(row['Product Weight']).replace(',', '.') + ' ' + 'kg',
+                    dataProductsInfo = {'prodDescription': str(redProdDescr[0:52]), 'prodQuantity': str(row['quantity-purchased']), 'prodWeight': str(row['Product Weight']).replace(',', '.') + ' ' + 'kg',
                                     'prodValue': '€' + ' ' + str(prodPriceIn), 'prodHSTarif': str(row['HS']), 'prodCountryOfOrigin': 'Greece'}
                     marketplace = 'amazon'
                 #ebay
                 elif df.columns[0] == 'Sales record number':
-                    dataReceiver = {'Name': str(row['Buyer full name']), 'Business': str(row['Buyer full name']), 'Street': str(row['Delivery address 1']),
+                    dataReceiver = {'Name': str(row['Buyer full name']), 'Business': '', 'Street': str(row['Delivery address 1']),
                                     'TelNo': str(row['Phone']), 'PostCode': str(row['Delivery postcode']), 'City': str(row['Delivery city']), 'Country': str(row['Delivery country'])}
                     redProdDescr = row['Item title']
                     prodPriceIn = "%.2f" % round(row['Total price'],2)
-                    dataProductsInfo = {'prodDescription': str(redProdDescr[0:25]), 'prodQuantity': str(row['Quantity']),
+                    dataProductsInfo = {'prodDescription': str(redProdDescr[0:52]), 'prodQuantity': str(row['Quantity']),
                                         'prodWeight': str(row['Product Weight']).replace(',', '.'), 'prodValue': str(prodPriceIn), 'prodHSTarif': str(row['HS']),
                                         'prodCountryOfOrigin': 'Greece'}
                     marketplace = 'ebay'
@@ -112,7 +113,7 @@ def CN23_PDF(userEmail, MarketPlaceId, cn23_idx, dataIn):
     # create a new PDF with Reportlab
     can = canvas.Canvas(packet, pagesize=letter)
 
-    fontSettings = [10, 8, 6, 4, 2]
+    fontSettings = [8, 5]
     can.setFont('Vera', fontSettings[0])
 
     'Sender Info'
@@ -131,22 +132,19 @@ def CN23_PDF(userEmail, MarketPlaceId, cn23_idx, dataIn):
     'Receiver Info'
     can.drawString(84, 714, dataReceiver['Name'])
     can.drawString(84, 700, dataReceiver['Business'])
+
+    if len(dataReceiver['Street']) >= 28:
+        can.setFont('Vera', fontSettings[1])
     can.drawString(80, 685, dataReceiver['Street'])
+
     can.drawString(225, 685, dataReceiver['TelNo'])
     can.drawString(120, 670, dataReceiver['PostCode'])
     can.drawString(200, 670, dataReceiver['City'])
     can.drawString(84, 655, dataReceiver['Country'])
     '1st Product info'
 
-    print(len(dataProductsInfo['prodDescription']))
-    if len(dataProductsInfo['prodDescription']) >= 20 and len(dataProductsInfo['prodDescription']) < 25:
+    if len(dataProductsInfo['prodDescription']) >= 20:
         can.setFont('Vera', fontSettings[1])
-    elif len(dataProductsInfo['prodDescription']) >= 25 and len(dataProductsInfo['prodDescription']) < 30:
-        can.setFont('Vera', fontSettings[1])
-    elif len(dataProductsInfo['prodDescription']) >= 30 and len(dataProductsInfo['prodDescription']) < 40:
-        can.setFont('Vera', fontSettings[2])
-    elif len(dataProductsInfo['prodDescription']) >= 40 and len(dataProductsInfo['prodDescription']) < 80:
-        can.setFont('Vera', fontSettings[3])
     can.drawString(54, 618, dataProductsInfo['prodDescription'])
 
     can.setFont('Vera', fontSettings[0])
@@ -203,6 +201,17 @@ def CN23_PDF(userEmail, MarketPlaceId, cn23_idx, dataIn):
     output.write(outputStream)
     outputStream.close()
 
+    'Clear UserUploads and CN23Out Folders'
+    UserUploadPath = ParentPath + '/' + userEmail + '/'
+    clearFolderData(UserUploadPath)
+    cn23Path = CN23_OutPath + '/' + userEmail + '/'
+    clearFolderData(cn23Path)
+
+def clearFolderData(mypath):
+    for root, dirs, files in os.walk(mypath):
+        for file in files:
+            os.remove(os.path.join(root, file))
+
 def sendGridEmail():
 
     client = sendgrid.SendGridClient("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")
@@ -219,5 +228,7 @@ def sendGridEmail():
 #    app.run(debug=True, port=2000)
 
 #fetchWixData('50d16d4b6a84952cd160c461b4aafd92409427cdf47d17c89c004f3c0cf332426ef75ca954115ef6b4ca79a7f9e694911e60994d53964e647acf431e4f798bcd5df106f0a689304605995f23566f925e02c1cb1fbfd9d63a2968755a2cf81e5d')
-fetchWixData('bf03dd816bf20d2dec142f9ba4e9cec682908a42145b0d17c57cc89498ca8770ec760030e230d62a29cf0b1c305a52861e60994d53964e647acf431e4f798bcd75fe9676af4e71b77777835b55c1c543ba2d300f6325b6d634ee736961ec9c5f')
+#fetchWixData('bf03dd816bf20d2dec142f9ba4e9cec682908a42145b0d17c57cc89498ca8770ec760030e230d62a29cf0b1c305a52861e60994d53964e647acf431e4f798bcd75fe9676af4e71b77777835b55c1c543ba2d300f6325b6d634ee736961ec9c5f')
 #sendGridEmail()
+
+
